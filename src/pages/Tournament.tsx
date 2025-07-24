0737f8e00170db0dce02d70f04b57af68b59c175
targoncup <targoncupofficial@gmail.com>
2025-07-24 18:55:36 +0200

    Remove extra closing div tag in Tournament.tsx

--- src/pages/Tournament.tsx
+++ src/pages/Tournament.tsx
@@ -99,7 +99,6 @@ const Tournament: React.FC = () => {
                     </div>
                   </div>
                 </div>
-              </div>
             </div>
           </div>
         </div>
